0bfb59d041204d2566e583e7551d24b9f65c8b44
rakeshkandhi <rakeshkandhi1432@gmail.com>
2026-07-31 23:34:58 +0530

    feat(keymaps): add shortcuts for built-in spellcheck (leader ss, leader sa, leader st)

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,6 +1,5 @@
 vim.keymap.set("n", "gl", vim.diagnostic.open_float)
 
-
 vim.keymap.set("n", "<Esc>", function()
 	vim.cmd("nohlsearch")
 	vim.cmd("echo ''")
@@ -9,8 +8,6 @@ end)
 -- =========================
 -- VSCode-like Developer Keymaps
 -- =========================
-
-
 
 -- Move lines up and down (Alt+j/k in Normal, Insert, Visual modes)
 -- Normal Mode
@@ -23,4 +20,9 @@ vim.keymap.set("i", "<A-k>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Line Up" 
 vim.keymap.set("v", "<A-j>", ":m '>+1<cr>gv=gv", { desc = "Move Selection Down" })
 vim.keymap.set("v", "<A-k>", ":m '<-2<cr>gv=gv", { desc = "Move Selection Up" })
 
-
+-- -- =========================
+-- -- Built-in Spell Check Shortcuts
+-- -- =========================
+-- vim.keymap.set("n", "<leader>ss", "z=", { desc = "Spelling Suggestions (z=)" })
+-- vim.keymap.set("n", "<leader>sa", "zg", { desc = "Add Word to Dict (zg)" })
+-- vim.keymap.set("n", "<leader>st", "<cmd>set spell!<cr>", { desc = "Toggle Red Underline Spell Check" })
