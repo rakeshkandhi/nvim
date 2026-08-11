ea65f2445f2e6e2f31ad34c4effc6be76c8e8db3
rakeshkandhi <rakeshkandhi1432@gmail.com>
2026-08-11 23:32:07 +0530

    perf: add tpipeline statusline restore and clear options

--- lua/config/plugins/tpipeline.lua
+++ lua/config/plugins/tpipeline.lua
@@ -6,6 +6,8 @@ return {
 			vim.opt.laststatus = 0
 			vim.g.tpipeline_autoembed = 1
 			vim.g.tpipeline_statusline = ""
+			vim.g.tpipeline_restore = 1
+			vim.g.tpipeline_clear = 1
 		end
 	end,
 }
